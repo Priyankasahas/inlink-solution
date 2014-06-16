6e30346226043a08320e3822f41a7ef14d151a12
Priyanka <priyanka@greenenergytrading.com.au>
2014-06-16 17:00:17 +1000

    Update to application config for asset pre-compilation on Rails 4.

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module InlinkOnline
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    config.serve_static_assets = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
